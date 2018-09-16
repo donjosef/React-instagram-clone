facca4a5cdef496db5dc4f19f896e31039c7d287
donjoe <giumontanaro@gmail.com>
2018-09-16 15:02:04 +0200

    extending authentication functionality returning login if not authenticated

--- src/hoc/Authentication/Authentication.js
+++ src/hoc/Authentication/Authentication.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import Login from '../../components/Login/Login'
 
 const authentication = WrappedComponent => {
-  return class extends Component {
-    render() {
-      return <WrappedComponent {...this.props}/>
+    return class extends Component {
+      state = {
+        loggedIn: false
+      }
+
+      componentDidMount() {
+        if(localStorage.getItem('username')) {
+          this.setState({
+            loggedIn: true
+          })
+        } else {
+          this.setState({
+            loggedIn: false
+          })
+        }
+      }
+      render() {
+        if(!this.state.loggedIn) {
+          return <Login />
+        } else {
+          return <WrappedComponent />
+        }
+      }
     }
-  }
 }
 
 export default authentication;
